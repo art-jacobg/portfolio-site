8f5aa3e832542cb569b7d92fd6c1b14fd447a933
art-jacobg <jacobfgordillo@gmail.com>
2026-06-04 19:06:38 +0000

    KILLY OURSELF

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,14 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
-	vite: {
-		ssr: {
-			external: ["svgo"],
-		},
-	},
+  site: "https://art-jacobg.com",
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
 });
