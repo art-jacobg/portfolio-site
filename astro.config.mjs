3b2f4ee73943a8eea14cce19ec571569af6cc543
art-jacobg <jacobfgordillo@gmail.com>
2026-06-05 00:25:56 +0000

    FINAL!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://art-jacobg.com",
   integrations: [mdx(), sitemap()],
