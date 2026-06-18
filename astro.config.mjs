fc2726c331c99adc4814e3975d6c801501f0f995
art-jacobg <jacobfgordillo@gmail.com>
2026-06-18 10:51:04 +0000

    feat: working astro cloudflare build with all content and assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  prefetch: true,
+  site: "https://art-jacobg.com",
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+});
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Art-JacobG";
+export const SITE_DESCRIPTION = "Jacob Gordillo - Designer and Illustrator";
